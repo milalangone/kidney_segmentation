091fd08c83ffda6ecd70bee748d4f458fee15c2d
milalangone <mlangone@itba.edu.ar>
2023-11-14 18:00:24 -0300

    horizontal layout

--- GUI_kidneys.py
+++ GUI_kidneys.py
@@ -1,40 +1,35 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog
+from PyQt5.QtWidgets import QApplication, QSplitter, QVBoxLayout, QWidget, QLabel, QPushButton, QFileDialog
 from PyQt5.QtGui import QPixmap
-import cv2
-import pandas as pd
-import numpy as mp
 
-class KidneyClassifierApp(QMainWindow):
+class KidneyClassifierApp(QSplitter):
     def __init__(self):
-        super().__init__()
+        super().__init__(Qt.Horizontal)
         self.initUI()
 
     def initUI(self):
-        # Create the main window
-        self.setWindowTitle("Kidney CT Image Classifier")
-        self.setGeometry(100, 100, 800, 600)
+        # Create widgets for the left half
+        left_widget = QWidget()
+        left_layout = QVBoxLayout(left_widget)
+        self.upload_button = QPushButton("Upload Image", left_widget)
+        self.image_label = QLabel(left_widget)
+        left_layout.addWidget(self.upload_button)
+        left_layout.addWidget(self.image_label)
 
-        # Create widgets
-        self.image_label = QLabel(self)
-        self.upload_button = QPushButton("Upload Image", self)
-        self.process_button = QPushButton("Process Image", self)
-        self.classify_button = QPushButton("Classify Image", self)
-        self.result_label = QLabel(self)
+        # Create widgets for the right half
+        right_widget = QWidget()
+        right_layout = QVBoxLayout(right_widget)
+        self.process_button = QPushButton("Process Image", right_widget)
+        self.classify_button = QPushButton("Classify Image", right_widget)
+        right_layout.addWidget(self.process_button)
+        right_layout.addWidget(self.classify_button)
 
-        # Set up the layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.upload_button)
-        layout.addWidget(self.image_label)
-        layout.addWidget(self.process_button)
-        layout.addWidget(self.classify_button)
-        layout.addWidget(self.result_label)
-
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
+        # Add the left and right widgets to the splitter
+        self.addWidget(left_widget)
+        self.addWidget(right_widget)
 
         # Connect button signals to functions
+        ##hola
         self.upload_button.clicked.connect(self.upload_image)
         self.process_button.clicked.connect(self.process_image)
         self.classify_button.clicked.connect(self.classify_image)
@@ -58,7 +53,6 @@ class KidneyClassifierApp(QMainWindow):
         pixmap = QPixmap(file_path)
         self.image_label.setPixmap(pixmap.scaled(self.image_label.size(), aspectRatioMode=Qt.KeepAspectRatio))
 
-    
     def process_image(self):
         # Implement image processing logic here
         pass
