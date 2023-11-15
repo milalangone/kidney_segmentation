e255b40bfefefbaeaa6c7dd2439312d2ec8d666e
milalangone <mlangone@itba.edu.ar>
2023-11-14 21:02:27 -0300

    limit to two rectangles -> bug: plot

--- GUI_kidneys.py
+++ GUI_kidneys.py
@@ -1,6 +1,10 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QSplitter, QVBoxLayout, QWidget, QLabel, QPushButton, QFileDialog
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QRubberBand, QSplitter, QVBoxLayout, QWidget, QLabel, QPushButton, QFileDialog
+from PyQt5.QtGui import QImage, QPainter, QPixmap
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsRectItem, QMainWindow
+from PyQt5.QtCore import QByteArray, QEvent, QRect, Qt, QRectF
+import numpy as np
+import cv2
+
 
 class KidneyClassifierApp(QSplitter):
     def __init__(self):
@@ -21,8 +25,12 @@ class KidneyClassifierApp(QSplitter):
         right_layout = QVBoxLayout(right_widget)
         self.process_button = QPushButton("Process Image", right_widget)
         self.classify_button = QPushButton("Classify Image", right_widget)
+        self.rect1_label = QLabel(right_widget)
+        self.rect2_label = QLabel(right_widget)
         right_layout.addWidget(self.process_button)
         right_layout.addWidget(self.classify_button)
+        right_layout.addWidget(self.rect1_label)
+        right_layout.addWidget(self.rect2_label)
 
         # Add the left and right widgets to the splitter
         self.addWidget(left_widget)
@@ -33,6 +41,8 @@ class KidneyClassifierApp(QSplitter):
         self.upload_button.clicked.connect(self.upload_image)
         self.process_button.clicked.connect(self.process_image)
         self.classify_button.clicked.connect(self.classify_image)
+
+    
 
     def upload_image(self):
         options = QFileDialog.Options()
@@ -47,12 +57,92 @@ class KidneyClassifierApp(QSplitter):
         )
 
         if file_name:
-            self.display_image(file_name)
+            self.img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
+            self.display_image(file_name)     
+
 
     def display_image(self, file_path):
         pixmap = QPixmap(file_path)
         self.image_label.setPixmap(pixmap.scaled(self.image_label.size(), aspectRatioMode=Qt.KeepAspectRatio))
+        self.selected_rectangles = []
+        self.create_rectangle_selector(self.image_label)
+        
 
+    def process_selected_rectangle(self, top_left_x,top_left_y, bottom_right_x,bottom_right_y):
+        self.imgs = []
+        if len(self.selected_rectangles) < 2 : 
+            self.selected_rectangles.append(self.selected_rect)
+            self.new_image = np.zeros((bottom_right_x-top_left_x, bottom_right_y-top_left_y))
+            for i in range(self.new_image.shape[0]):
+                for j in range(self.new_image.shape[1]):
+                    self.new_image[i,j] = self.img[top_left_x + i , bottom_right_y + j]
+            self.imgs.append(self.new_image)
+            self.update_image_label()
+
+
+    def update_image_label(self):
+        # Create a copy of the original image
+        original_pixmap = self.image_label.pixmap()
+
+        # Create a new painter to draw the selected rectangles on the image
+        painter = QPainter(original_pixmap)
+
+        # Draw each selected rectangle on the image
+        for selected_rect in self.selected_rectangles:
+            painter.drawRect(selected_rect)
+
+        # Update the image label with the modified image
+        self.image_label.setPixmap(original_pixmap)
+
+        qimage_rect1 = QImage(self.imgs[0], self.imgs[0].shape[1], self.imgs[0].shape[0],QImage.Format_Grayscale8)                                                                                                                                                                 
+        pixmap_rect1 = QPixmap(qimage_rect1)                                                                                                                                                                               
+        pixmap_rect1 = pixmap_rect1.scaled(640,400, Qt.KeepAspectRatio)                                                                                                                                                    
+        self.rect1_label.setPixmap(pixmap_rect1)
+
+        if len(self.imgs) == 2:
+            qimage_rect2 = QImage(self.imgs[1], self.imgs[1].shape[1], self.imgs[1].shape[0],QImage.Format_Grayscale8)                                                                                                                                                                 
+            pixmap_rect2 = QPixmap(qimage_rect2)                                                                                                                                                                               
+            pixmap_rect2 = pixmap_rect2.scaled(640,400, Qt.KeepAspectRatio)   
+            self.rect2_label.setPixmap(pixmap_rect2)
+
+    def create_rectangle_selector(self, image_view):
+        def mousePressEvent(event):
+            if event.button() == Qt.MouseButton.LeftButton:
+                self.start_pos = event.pos()
+                self.rubber_band = QRubberBand(QRubberBand.Rectangle, image_view)
+                self.rubber_band.show()
+                self.image_label.update()
+
+        def mouseMoveEvent(event):
+            if self.rubber_band:
+                self.rubber_band.setGeometry(
+                    QRect(self.start_pos, event.pos()).normalized()
+                )
+                self.image_label.update()
+
+        def mouseReleaseEvent(event):
+            if self.rubber_band:
+                self.rubber_band.hide()
+                self.rubber_band.deleteLater()
+                self.rubber_band = None
+                self.image_label.update()
+
+                # Get the selected rectangle
+                self.selected_rect = QRectF(self.start_pos, event.pos()).normalized()
+                top_left_x = int(self.selected_rect.x())
+                top_left_y = int(self.selected_rect.y())
+                bottom_right_x = int(self.selected_rect.right())
+                bottom_right_y = int(self.selected_rect.bottom())
+
+                # Process the selected rectangle
+                self.process_selected_rectangle(top_left_x,top_left_y, bottom_right_x,bottom_right_y)
+                
+
+        image_view.mousePressEvent = mousePressEvent
+        image_view.mouseMoveEvent = mouseMoveEvent
+        image_view.mouseReleaseEvent = mouseReleaseEvent
+
+        
     def process_image(self):
         # Implement image processing logic here
         pass
