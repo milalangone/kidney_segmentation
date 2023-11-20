8cd9c402397386db55909a7da441982e01e4ca9c
milalangone <mlangone@itba.edu.ar>
2023-11-19 21:37:13 -0300

    changes in gui

--- GUI_kidneys.py
+++ GUI_kidneys.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QHBoxLayout, QRubberBand, QSplitter, QVBoxLayout, QWidget, QLabel, QPushButton, QFileDialog
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QRubberBand, QSizePolicy, QSpacerItem, QSplitter, QVBoxLayout, QWidget, QLabel, QPushButton, QFileDialog
 from PyQt5.QtGui import QImage, QPainter, QPixmap
 from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsRectItem, QMainWindow
 from PyQt5.QtCore import QByteArray, QEvent, QRect, Qt, QRectF
@@ -7,8 +8,11 @@ import cv2
 
 
 class KidneyClassifierApp(QSplitter):
-    def initUI(self):
+    def __init__(self):
+        super().__init__(Qt.Horizontal)
+        self.initUI()
         
+    def initUI(self):
         # Create widgets for the left half
         left_widget = QWidget()
         left_layout = QVBoxLayout(left_widget)
@@ -20,9 +24,18 @@ class KidneyClassifierApp(QSplitter):
         # Create widgets for the right half
         right_widget = QWidget()
         right_layout = QVBoxLayout(right_widget)
-        self.title_label = QLabel("Image Processing", right_widget)
-        self.title_label.setAlignment(Qt.AlignCenter)
-        right_layout.addWidget(self.title_label)
+        
+
+        # Create a vertical layout for classify and process buttons
+        buttons_layout = QVBoxLayout()
+
+        self.rect1_label = QLabel(right_widget)
+        self.rect2_label = QLabel(right_widget)
+        buttons_layout.addWidget(self.rect1_label)
+        buttons_layout.addWidget(self.rect2_label)
+
+        self.process_button = QPushButton("Process", right_widget)
+        buttons_layout.addWidget(self.process_button)
 
         # Create a horizontal layout for classify buttons
         classify_layout = QHBoxLayout()
@@ -31,9 +44,12 @@ class KidneyClassifierApp(QSplitter):
         self.classify_button2 = QPushButton("Classify Image 2", right_widget)
         classify_layout.addWidget(self.classify_button1)
         classify_layout.addWidget(self.classify_button2)
-        classify_layout.setAlignment(Qt.AlignTop)  # Align buttons to the top
 
-        right_layout.addLayout(classify_layout)
+        # Add the classify buttons to the vertical layout
+        buttons_layout.addLayout(classify_layout)
+
+        # Add the vertical layout to the right layout
+        right_layout.addLayout(buttons_layout)
 
         # Set font for buttons
         font = self.upload_button.font()
@@ -43,10 +59,6 @@ class KidneyClassifierApp(QSplitter):
         self.classify_button1.setFont(font)
         self.classify_button2.setFont(font)
 
-        self.rect1_label = QLabel(right_widget)
-        self.rect2_label = QLabel(right_widget)
-        right_layout.addWidget(self.rect1_label)
-        right_layout.addWidget(self.rect2_label)
 
         # Add the left and right widgets to the splitter
         self.addWidget(left_widget)
@@ -57,7 +69,6 @@ class KidneyClassifierApp(QSplitter):
         self.process_button.clicked.connect(self.process_image)
         self.classify_button1.clicked.connect(self.classify_image)
         self.classify_button2.clicked.connect(self.classify_image)
-
     
 
     def upload_image(self):
@@ -82,48 +93,8 @@ class KidneyClassifierApp(QSplitter):
         self.image_label.setPixmap(pixmap.scaled(self.image_label.size(), aspectRatioMode=Qt.KeepAspectRatio))
         self.selected_rectangles = []
         self.create_rectangle_selector(self.image_label)
-        
 
     
-    def process_selected_rectangle(self, top_left_x,top_left_y, bottom_right_x,bottom_right_y):
-        self.imgs = []
-        if len(self.selected_rectangles) < 2 : 
-            self.selected_rectangles.append(self.selected_rect)
-            height, width = bottom_right_y - top_left_y, bottom_right_x - top_left_x
-            self.new_image = np.zeros((height, width))
-            for i in range(height):
-                for j in range(width):
-                    # Swap indices for robustness
-                    self.new_image[i, j] = self.img[top_left_y + i, top_left_x + j]
-            self.imgs.append(self.new_image)
-            self.update_image_label()
-
-
-    def update_image_label(self):
-        # Create a copy of the original image
-        original_pixmap = self.image_label.pixmap()
-
-        # Create a new painter to draw the selected rectangles on the image
-        painter = QPainter(original_pixmap)
-
-        # Draw each selected rectangle on the image
-        for selected_rect in self.selected_rectangles:
-            painter.drawRect(selected_rect)
-
-        # Update the image label with the modified image
-        self.image_label.setPixmap(original_pixmap)
-
-        qimage_rect1 = QImage(self.imgs[0], self.imgs[0].shape[1], self.imgs[0].shape[0],QImage.Format_Grayscale8)                                                                                                                                                                 
-        pixmap_rect1 = QPixmap(qimage_rect1)                                                                                                                                                                               
-        pixmap_rect1 = pixmap_rect1.scaled(640,400, Qt.KeepAspectRatio)                                                                                                                                                    
-        self.rect1_label.setPixmap(pixmap_rect1)
-
-        if len(self.imgs) == 2:
-            qimage_rect2 = QImage(self.imgs[1], self.imgs[1].shape[1], self.imgs[1].shape[0], QImage.Format_Grayscale8)                                                                                                                                                                 
-            pixmap_rect2 = QPixmap(qimage_rect2)                                                                                                                                                                               
-            pixmap_rect2 = pixmap_rect2.scaled(640, 400, Qt.KeepAspectRatio)   
-            self.rect2_label.setPixmap(pixmap_rect2)
-
     def create_rectangle_selector(self, image_view):
         def mousePressEvent(event):
             if event.button() == Qt.MouseButton.LeftButton:
@@ -147,7 +118,7 @@ class KidneyClassifierApp(QSplitter):
                 self.image_label.update()
 
                 # Get the selected rectangle
-                self.selected_rect = QRectF(self.start_pos, event.pos()).normalized()
+                self.selected_rect = QRectF(self.start_pos, event.pos())
                 top_left_x = int(self.selected_rect.x())
                 top_left_y = int(self.selected_rect.y())
                 bottom_right_x = int(self.selected_rect.right())
@@ -160,6 +131,42 @@ class KidneyClassifierApp(QSplitter):
         image_view.mousePressEvent = mousePressEvent
         image_view.mouseMoveEvent = mouseMoveEvent
         image_view.mouseReleaseEvent = mouseReleaseEvent
+        
+
+    def process_selected_rectangle(self, top_left_x,top_left_y, bottom_right_x,bottom_right_y):
+        self.imgs = []
+        if len(self.selected_rectangles) < 2 : 
+            self.selected_rectangles.append(self.selected_rect)
+            height, width = bottom_right_y - top_left_y, bottom_right_x - top_left_x
+            self.new_image = np.zeros((height, width))
+            for i in range(height):
+                for j in range(width):
+                    # Swap indices for robustness
+                    self.new_image[i, j] = self.img[top_left_x + i, top_left_y + j]
+            self.imgs.append(self.new_image)
+            self.update_image_label()
+
+
+    def update_image_label(self):
+        # Create a copy of the original image
+        original_pixmap = self.image_label.pixmap()
+
+        # Create a new painter to draw the selected rectangles on the image
+        painter = QPainter(original_pixmap)
+
+        # Draw each selected rectangle on the image
+        for selected_rect in self.selected_rectangles:
+            painter.drawRect(selected_rect)
+
+        # Update the image label with the modified image
+        self.image_label.setPixmap(original_pixmap)
+
+        qimage_rect1 = QImage(self.imgs[0], self.imgs[0].shape[1], self.imgs[0].shape[0],QImage.Format_Grayscale8)                                                                                                                                                                                                                                                                                                                
+        self.rect1_label.setPixmap(QtGui.QPixmap.fromImage(qimage_rect1))
+
+        if len(self.imgs) == 2:
+            qimage_rect2 = QImage(self.imgs[1], self.imgs[1].shape[1], self.imgs[1].shape[0], QImage.Format_Grayscale8)                                                                                                                                                                 
+            self.rect2_label.setPixmap(QtGui.QPixmap.fromImage(qimage_rect2))
 
         
     def process_image(self):
